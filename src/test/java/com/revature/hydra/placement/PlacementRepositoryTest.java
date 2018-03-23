a29e280565cb5c1a6a5de0766a2c763d7478c7cb
carsonzjq <carson.zjq@gmail.com>
2018-03-23 15:23:55 -0400

    everything

--- src/test/java/com/revature/hydra/placement/PlacementRepositoryTest.java
+++ src/test/java/com/revature/hydra/placement/PlacementRepositoryTest.java
@@ -31,101 +31,96 @@ import com.revature.hydra.placement.data.PlacementRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = PlacementRepositoryServiceApplication.class)
 public class PlacementRepositoryTest {
-	
-	@Test
-	public void helloWorld() {
-		System.out.println("Hello world!");
+
+	private static final Logger log = Logger.getLogger(PlacementRepositoryTest.class);
+
+	@Autowired
+	PlacementRepository placementRepository;
+
+	Placement testPlacement;
+
+	@Before
+	public void init() {
+		log.info("Initializing a test placement object for testing.");
+		testPlacement = new Placement(1000, new Timestamp(100_000), new Timestamp(200_000), 5501, 5501, 5505);
+		testPlacement = placementRepository.save(testPlacement);
 	}
 
-//	private static final Logger log = Logger.getLogger(PlacementRepositoryTest.class);
-//
-//	@Autowired
-//	PlacementRepository placementRepository;
-//
-//	Placement testPlacement;
-//
-//	@Before
-//	public void init() {
-//		log.info("Initializing a test placement object for testing.");
-//		testPlacement = new Placement(1000, new Timestamp(100_000), new Timestamp(200_000), 5501, 5501, 5505);
-//		testPlacement = placementRepository.save(testPlacement);
-//	}
-//
-//	@After
-//	public void teardown() {
-//		log.info("Tear down");
-//		if (placementRepository.findOneByPlacementId(testPlacement.getPlacementId()) != null) {
-//			placementRepository.delete(testPlacement);
-//		}
-//	}
-//
-//	@Test
-//	public void addPlacement() {
-//		log.info("Test adding a placement.");
-//		testPlacement = new Placement(1001, new Timestamp(100_000), new Timestamp(200_000), 5510, 5510, 5503);
-//		Placement savedPlacement = placementRepository.save(testPlacement);
-//
-//		assertTrue(placementRepository.findAll().contains(savedPlacement));
-//	}
-//
-//	@Test
-//	public void findOneByPlacmentId() {
-//		log.info("Test getting a placement by placementId.");
-//		Placement placement = placementRepository.findOneByPlacementId(testPlacement.getPlacementId());
-//
-//		assertEquals(testPlacement, placement);
-//	}
-//
-//	@Test
-//	public void findAll() {
-//		log.info("Test getting all placements.");
-//		List<Placement> placements = placementRepository.findAll();
-//
-//		assertFalse(placements.isEmpty());
-//	}
-//
-//	@Test
-//	public void findAllByAssociateId() {
-//		log.info("Test getting placements by AssociateId");
-//		List<Placement> placements = placementRepository.findAllByAssociateId(testPlacement.getAssociateId());
-//
-//		assertNotNull(placements);
-//		assertTrue(placements.contains(testPlacement));
-//	}
-//
-//	@Test
-//	public void findAllByClientId() {
-//		log.info("Test getting placements by ClientId");
-//		List<Placement> placements = placementRepository.findAllByClientId(testPlacement.getClientId());
-//
-//		assertNotNull(placements);
-//		assertTrue(placements.contains(testPlacement));
-//	}
-//
-//	@Test
-//	public void findAllByEndClientId() {
-//		log.info("Test getting placements by EndClientId");
-//		List<Placement> placements = placementRepository.findAllByEndClientId(testPlacement.getEndClientId());
-//
-//		assertNotNull(placements);
-//		assertTrue(placements.contains(testPlacement));
-//	}
-//
-//	@Test
-//	public void updatePlacement() {
-//		log.info("Test updating a placement.");
-//		testPlacement.setAssociateId(5507);
-//		Placement updatedPlacement = placementRepository.save(testPlacement);
-//
-//		assertEquals(updatedPlacement.getAssociateId(), testPlacement.getAssociateId());
-//	}
-//
-//	@Test
-//	public void deletePlacement() {
-//		log.info("Test deleting a placement.");
-//		placementRepository.delete(testPlacement);
-//
-//		assertNull(placementRepository.findOneByPlacementId(testPlacement.getPlacementId()));
-//	}
+	@After
+	public void teardown() {
+		log.info("Tear down");
+		if (placementRepository.findOneByPlacementId(testPlacement.getPlacementId()) != null) {
+			placementRepository.delete(testPlacement);
+		}
+	}
+
+	@Test
+	public void addPlacement() {
+		log.info("Test adding a placement.");
+		testPlacement = new Placement(1001, new Timestamp(100_000), new Timestamp(200_000), 5510, 5510, 5503);
+		Placement savedPlacement = placementRepository.save(testPlacement);
+
+		assertTrue(placementRepository.findAll().contains(savedPlacement));
+	}
+
+	@Test
+	public void findOneByPlacmentId() {
+		log.info("Test getting a placement by placementId.");
+		Placement placement = placementRepository.findOneByPlacementId(testPlacement.getPlacementId());
+
+		assertEquals(testPlacement, placement);
+	}
+
+	@Test
+	public void findAll() {
+		log.info("Test getting all placements.");
+		List<Placement> placements = placementRepository.findAll();
+
+		assertFalse(placements.isEmpty());
+	}
+
+	@Test
+	public void findAllByAssociateId() {
+		log.info("Test getting placements by AssociateId");
+		List<Placement> placements = placementRepository.findAllByAssociateId(testPlacement.getAssociateId());
+
+		assertNotNull(placements);
+		assertTrue(placements.contains(testPlacement));
+	}
+
+	@Test
+	public void findAllByClientId() {
+		log.info("Test getting placements by ClientId");
+		List<Placement> placements = placementRepository.findAllByClientId(testPlacement.getClientId());
+
+		assertNotNull(placements);
+		assertTrue(placements.contains(testPlacement));
+	}
+
+	@Test
+	public void findAllByEndClientId() {
+		log.info("Test getting placements by EndClientId");
+		List<Placement> placements = placementRepository.findAllByEndClientId(testPlacement.getEndClientId());
+
+		assertNotNull(placements);
+		assertTrue(placements.contains(testPlacement));
+	}
+
+	@Test
+	public void updatePlacement() {
+		log.info("Test updating a placement.");
+		testPlacement.setAssociateId(5507);
+		Placement updatedPlacement = placementRepository.save(testPlacement);
+
+		assertEquals(updatedPlacement.getAssociateId(), testPlacement.getAssociateId());
+	}
+
+	@Test
+	public void deletePlacement() {
+		log.info("Test deleting a placement.");
+		placementRepository.delete(testPlacement);
+
+		assertNull(placementRepository.findOneByPlacementId(testPlacement.getPlacementId()));
+	}
 
 }
